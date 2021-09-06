ebf3c556cab423fbaa9eb4324224a89fc14391ea
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-06 11:06:50 -0300

    refactor: separate the application's routes, in specific files of each route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(routes)
 
 app.listen(3333, () => {
-  console.log(' Server started on port 3333!!!')
+  console.log(' 🪐👽🪐 Server started on port 3333!!! 🦅🐲')
 })
