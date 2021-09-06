449916a1384937dc99d7364f55f05609b28dbf94
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-06 17:30:26 -0300

    feat: add relation with user and appointment entities

--- backend/src/database/migrations/1630950795197-CreateAppointments.ts
+++ backend/src/database/migrations/1630950795197-CreateAppointments.ts
@@ -10,7 +10,7 @@ export default class CreateAppointments1630950795197
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()'
