9a8080976f4238891c55fe51f0cb777973b4f35f
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-06 11:50:57 -0300

    chore: add date-fns lib

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,9 +1,26 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
 
 const appointmentsRouter = Router()
 
+const appointments = []
+
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
+  const { provider, date } = request.body
+
+  console.log(provider)
+
+  const appointment = {
+    id: v4(),
+    provider,
+    date
+  }
+
+  appointments.push(appointment)
+
+  console.log(appointments)
+
+  return response.json(appointment)
 })
 
 export default appointmentsRouter
