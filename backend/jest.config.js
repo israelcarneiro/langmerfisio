10806fea4c5ead81c3d45c8b77e35578e843cada
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-15 16:55:19 -0300

    test:  should be able to create a new apppointment

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,9 +1,6 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
-export default {
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig.json')
+module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -31,7 +28,7 @@ export default {
   // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
+  coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -81,7 +78,7 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/'}),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -137,7 +134,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+  testEnvironment: "jest-environment-node",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -146,7 +143,9 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/*.spec.ts']
+  testMatch: [
+    "**/*.spec.ts"
+  ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -188,4 +187,4 @@ export default {
 
   // Whether to use watchman for file crawling
   // watchman: true,
-}
+};
---
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,22 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService'
+
+describe('CreateAppointments', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository()
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    )
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123'
+    })
+
+    expect(appointment).toHaveProperty('id')
+    expect(appointment.provider_id).toBe('123123')
+  })
+  // it('should be not able to create two appointments on the same time', () => {
+
+  // })
+})
