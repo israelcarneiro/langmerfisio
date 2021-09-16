60814c7996ec1652b6babbdcee4b9d3b3f8e8bda
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-16 09:34:25 -0300

    test: should not be able to create a new user with same email from another

--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -1,4 +1,4 @@
-// import AppError from '@shared/errors/AppError'
+import AppError from '@shared/errors/AppError'
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
 import CreateUserService from './CreateUserService'
@@ -15,5 +15,24 @@ describe('CreateUser', () => {
     })
 
     expect(user).toHaveProperty('id')
+  })
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const createUser = new CreateUserService(fakeUsersRepository)
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123123'
+    })
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123123'
+      })
+    ).rejects.toBeInstanceOf(AppError)
   })
 })
