a1fe93129e14eda7be9330a10ac1318757ddacd4
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-16 10:49:46 -0300

    test: should not be able to update avatar from non existing user

--- backend/src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ backend/src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -1,4 +1,4 @@
-// import AppError from '@shared/errors/AppError'
+import AppError from '@shared/errors/AppError'
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
 import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider'
@@ -25,5 +25,22 @@ describe('UpdateUserAvatar', () => {
     })
 
     expect(user.avatar).toBe('avatar.jpg')
+  })
+
+  it('should not be able to update avatar from non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeStorageProvider = new FakeStorageProvider()
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider
+    )
+
+    await expect(
+      updateUserAvatar.execute({
+        user_id: 'non-existing-user',
+        avatarFilename: 'avatar.jpg'
+      })
+    ).rejects.toBeInstanceOf(AppError)
   })
 })
