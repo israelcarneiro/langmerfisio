e5e39add777837e5ddb37b4940b5dd2313faf6d0
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-16 20:37:52 -0300

    test(refactor): should be able to reset the password

--- backend/src/modules/users/services/ResetPasswordService.spec.ts
+++ backend/src/modules/users/services/ResetPasswordService.spec.ts
@@ -2,20 +2,24 @@ import AppError from '@shared/errors/AppError'
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
 import FakeUserTokensRepository from '../repositories/fakes/FakeUserTokensRepository'
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
 import ResetPasswordService from './ResetPasswordService'
 
 let fakeUsersRepository: FakeUsersRepository
 let fakeUserTokensRepository: FakeUserTokensRepository
+let fakeHashProvider: FakeHashProvider
 let resetPasswordService: ResetPasswordService
 
 describe('SendForgotPasswordEmail', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository()
     fakeUserTokensRepository = new FakeUserTokensRepository()
+    fakeHashProvider = new FakeHashProvider()
 
     resetPasswordService = new ResetPasswordService(
       fakeUsersRepository,
-      fakeUserTokensRepository
+      fakeUserTokensRepository,
+      fakeHashProvider
     )
   })
 
@@ -26,6 +30,8 @@ describe('SendForgotPasswordEmail', () => {
       password: '123456'
     })
 
+    const generateHash = jest.spyOn(fakeHashProvider, 'generateHash')
+
     const { token } = await fakeUserTokensRepository.generate(user.id)
 
     await resetPasswordService.execute({
@@ -35,6 +41,7 @@ describe('SendForgotPasswordEmail', () => {
 
     const updatedUser = await fakeUsersRepository.findById(user.id)
 
+    expect(generateHash).toHaveBeenCalledWith('123123')
     expect(updatedUser?.password).toBe('123123')
   })
 })
