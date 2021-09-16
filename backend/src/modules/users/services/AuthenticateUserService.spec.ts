47f470dbedbf896ae205e011c83169789a1a3fc1
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-16 10:09:20 -0300

    test: should not be able to authenticate with non existing user

--- backend/src/modules/users/services/AuthenticateUserService.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,4 +1,4 @@
-// import AppError from '@shared/errors/AppError'
+import AppError from '@shared/errors/AppError'
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider'
@@ -33,5 +33,22 @@ describe('AuthenticateUser', () => {
 
     expect(response).toHaveProperty('token')
     expect(response.user).toEqual(user)
+  })
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const fakeHashProvider = new FakeHashProvider()
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    )
+
+    await expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '123123'
+      })
+    ).rejects.toBeInstanceOf(AppError)
   })
 })
