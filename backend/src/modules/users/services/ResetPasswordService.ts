ad2ae1506ba41302d98b80d6004a1f8d88a92c7c
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-16 20:38:08 -0300

    feat: should be able to reset the password

--- backend/src/modules/users/services/ResetPasswordService.ts
+++ backend/src/modules/users/services/ResetPasswordService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe'
 import AppError from '@shared/errors/AppError'
 import IUsersRepository from '../repositories/IUsersRepository'
 import IUserTokensRepository from '../repositories/IUserTokensRepository'
+import IHashProvider from '../providers/HashProvider/models/IHashProvider'
 // import User from '@modules/users/infra/typeorm/entities/User'
 
 interface IRequest {
@@ -17,7 +18,10 @@ class ResetPasswordService {
     private usersRepository: IUsersRepository,
 
     @inject('UserTokensRepository')
-    private userTokensRepository: IUserTokensRepository
+    private userTokensRepository: IUserTokensRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider
   ) {}
 
   public async execute({ token, password }: IRequest): Promise<void> {
@@ -33,7 +37,7 @@ class ResetPasswordService {
       throw new AppError('User does not exists')
     }
 
-    user.password = password
+    user.password = await this.hashProvider.generateHash(password)
 
     await this.usersRepository.save(user)
   }
