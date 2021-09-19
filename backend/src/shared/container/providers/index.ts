5b9d46eede92f47075468880b0d8de5c7d327ec6
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-19 19:00:05 -0300

    feat: should able to send emails in dev env with ethereal, and reset password feat working with successfully

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,9 +1,17 @@
 import { container } from 'tsyringe'
 
+import IMailProvider from './MailProvider/models/IMailProvider'
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider'
+
 import IStorageProvider from './StorageProvider/models/IStorageProvider'
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider'
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider
+)
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider()
 )
