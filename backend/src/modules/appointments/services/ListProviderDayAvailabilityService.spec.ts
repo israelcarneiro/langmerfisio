368be7204ef51957f78a990f0ef656fbabf0336f
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-21 18:33:39 -0300

    refactor(test): should be able to list provider day availability

--- backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ backend/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -15,11 +15,13 @@ describe('ListProviderDayAvailability', () => {
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'any_provider_id',
+      user_id: 'any_user_id',
       date: new Date(2021, 8, 20, 14, 0, 0)
     })
 
     await fakeAppointmentsRepository.create({
       provider_id: 'any_provider_id',
+      user_id: 'any_user_id',
       date: new Date(2021, 8, 20, 16, 0, 0)
     })
 
