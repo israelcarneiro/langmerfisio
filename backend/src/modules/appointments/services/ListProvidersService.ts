01ed41657ccab751d21bfabf7b386af51c9ecd7e
Israel Carneiro <isragcarneiro@gmail.com>
2021-09-21 01:04:23 -0300

    feat: should be able to list the providers if the user is a provider

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe'
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 import User from '@modules/users/infra/typeorm/entities/User'
+import AppError from '@shared/errors/AppError'
 
 interface IRequest {
   user_id: string
@@ -15,6 +16,12 @@ class ListProvidersService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
+    const user = await this.usersRepository.findById(user_id)
+
+    if (user && user.is_provider === true) {
+      throw new AppError('Providers does not list anothers providers', 401)
+    }
+
     const users = await this.usersRepository.findAllProviders({
       except_user_id: user_id
     })
